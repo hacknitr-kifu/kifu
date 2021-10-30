3c0c58f31dc6622662a77d6662f845327dc87eeb
Sarthak <getsarthakaggarwal@gmail.com>
2021-10-31 00:48:47 +0530

    image classifier UI enhancement

--- app/kifu/lib/pages/image_classifier.dart
+++ app/kifu/lib/pages/image_classifier.dart
@@ -52,7 +52,6 @@ class _ImageClassifierState extends State<ImageClassifier> {
     );
     setState(() {
       _loading = false;
-      //Declare List _outputs in the class which will be used to show the classified classs name and confidence
       _outputs = output;
     });
   }
@@ -77,47 +76,51 @@ class _ImageClassifierState extends State<ImageClassifier> {
         title: const Text('Add items'),
         backgroundColor: Colors.green,
       ),
-      body: _loading
-          ? Container(
-              alignment: Alignment.center,
-              child: CircularProgressIndicator(),
-            )
-          : Container(
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _image == null ? Container() : Image.file(File(_image!.path)),
-                  SizedBox(
-                    height: 400,
-                  ),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.black),
+      body: SingleChildScrollView(
+        child: _loading
+            ? Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _image == null
+                        ? Container()
+                        : Image.file(File(_image!.path)),
+                    SizedBox(
+                      height: 200,
                     ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoutes.cartRoute);
-                    },
-                    child: Text('Click here to add this item to cart'),
-                  ),
-                  _outputs != null
-                      ? Text(
-                          '${_outputs![0]["label"]}',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20.0,
-                            background: Paint()..color = Colors.white,
-                          ),
-                        )
-                      : Container()
-                ],
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all<Color>(Colors.black),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.cartRoute);
+                      },
+                      child: Text('Click here to add this item to cart'),
+                    ),
+                    _outputs != null
+                        ? Text(
+                            '${_outputs![0]["label"]}',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                              background: Paint()..color = Colors.white,
+                            ),
+                          )
+                        : Container()
+                  ],
+                ),
               ),
-            ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _optiondialogbox,
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.lightGreen,
         child: Icon(Icons.image),
       ),
     );
@@ -129,7 +132,7 @@ class _ImageClassifierState extends State<ImageClassifier> {
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            backgroundColor: Colors.purple,
+            backgroundColor: Colors.green,
             content: SingleChildScrollView(
               child: ListBody(
                 children: <Widget>[
